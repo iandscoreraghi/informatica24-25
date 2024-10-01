0192b302c2691e7aad85eb0f98d21a22b6ac8c79
iandscoreraghi <ihab.raghi@galileiostiglia.edu.it>
2024-10-01 07:48:16 +0000

    es-0110.c

---
+++ informatica/01102024.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+intmain(){
+
+int num1=0;
+printf("iserisci il valore di num1");
+scanf("%d", &num1);
+
+if(num1%3==00){
+    if(num1%5)
+    ("num1 è divisibile sia per 3 che per 5");
+}
+    else{
+        printf("num1 è divisibile per 3 ma non per 5");
+    }
+        {
+            printf("num1 mnon è divisibile per 3");
+            return 0;
+
+    }
+
+}
+  
+ 
