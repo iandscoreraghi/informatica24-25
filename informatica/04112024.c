215f57ff7eeb2e4a23e4a147f4cac8861cbbf2e2
iandscoreraghi <ihab.raghi@galileiostiglia.edu.it>
2024-11-03 20:10:58 +0000

    esercizio

---
+++ informatica/04112024.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main(){
+   int scelta;
+   float monetaeur , monetadol;
+   float scambioeur;
+   float scambiodol;
+    
+    printf("--scegli dal menu in che modo cambiare la moneta--");
+    printf("\n 0- termina");
+    printf("\n 1-scambiodol");
+    printf("\n 2-scambioeur");
+    printf("\n");
+    
+    printf("scelta: ");
+    scanf("%d" &scelta);
+    
+    switch (scelta) {
+        case 1: {
+            printf("inserisci")
+        }
+}
