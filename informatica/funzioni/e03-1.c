5ea03e18452b03bda800d05e2a30e76757e8f820
iandscoreraghi <ihab.raghi@galileiostiglia.edu.it>
2025-02-09 17:54:41 +0000

    c

---
+++ informatica/funzioni/e03-1.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+void sequenzaCollatz(int *_n);
+
+int main(){
+    int n=0;
+    
+    do{
+    printf("inserisci un numero:");
+    scanf("%d", &n);
+    }while(n<=0);
+    sequenzaCollatz(&n);
+
+}
+void sequenzaCollatz(int*_n){
+    int cntn=1;
+    while(*_n!=1){
+        if(*_n%2==0){
+            *_n=*_n/2;
+            cntn+=1;
+            printf("%d\t", *_n);
+        }else{
+            *_n=(*_n*3)+1;
+            cntn+=1;
+            printf("%d\t", *_n);
+        }
+    }printf("\nla lunghezza della sequenza è di %d", cntn);
+}
