7659f69bd51b8238ac0fb64b938f75461a95e248
iandscoreraghi <ihab.raghi@galileiostiglia.edu.it>
2025-09-18 07:46:51 +0000

    .-

--- InformaticaQuarta /Esspirale.c
+++ InformaticaQuarta /Esspirale.c
@@ -1,15 +1,39 @@
 #include <stdio.h>
-#define RIGHE 4
-#define COLONNE 4
-
+#define N 4 
+void riempiMatrice(int mat[][N]);
+void stampaMatrice(int mat[][N]);
 
 int main(){
-    int mat [RIGHE][COLONNE];
-    int val=0;
-
-    for(int j=0; j<4; j++){
-        mat [0][j]= val++;
-        printf("%d ", mat [0][j]);
-
-    }
+    int mat [N][N];
+    
 }
+    void riempiMatrice(int mat[][N]){
+        int num=0;
+        int i;
+        int top=0, bottom=N-1;
+        int left=0, right=N-1;
+
+        while(num<=N*N){
+            for(i=left; i<=right; i++)
+                mat[top][i]= num+1;
+            top++;
+            for(i=top; i<=bottom; i++)
+                mat[i][right]= num+1;
+            right--;
+            for(i=right; i>=left; i--)
+                mat[bottom][i]=num+1,
+            bottom--;
+            for(i=bottom; i>=top; i--)
+                mat[i][left]=num+1;
+            left++;
+
+        }
+    }
+    void stampaMatrice(int mat[][N]){
+        for(int i=0; i<N; i++){
+            for(int j=0; j<N; j++){
+                printf("%d\t", mat[i][j]);
+            printf("\n");
+            }
+        }
+    }
